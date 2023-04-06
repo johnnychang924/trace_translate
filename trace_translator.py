7dc17a57c63944f9e5f412987ac7495f67b33982
johnnychang924 <johnny611247@gmail.com>
2023-04-06 22:23:49 +0800

    fix read before write bug、add bitmap mode for memory not enough

--- trace_translator.py
+++ trace_translator.py
@@ -1,3 +1,39 @@
+def Simple_to_ASCII_bitmap(input_file_path, output_file_path, sector_size):
+    write_sector_num = 0              # 總共寫入的sector數量(包含read before write)
+    write_option_num = 0            # 總共的write trace數量
+    read_sector_num = 0               # 總共讀取的sector數量
+    read_option_num = 0             # 總共的read trace數量
+    read_before_write_sector_num = 0  # 發生read before write的sector數量
+    biggest_sector_address = 0
+    with open(input_file_path, 'r') as input_file:
+        for row in input_file:
+            mode, s_LSA, offset = row.split()
+            biggest_sector_address = max(biggest_sector_address, int(s_LSA) + int(int(offset) / sector_size))
+    LSA_has_been_write = [False] * biggest_sector_address      # bitmap
+    with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
+        for i, row in enumerate(input_file):
+            mode, s_LSA, offset = row.split()
+            offset = int(int(offset) / sector_size) # size in sector
+            s_LSA = int(s_LSA)
+            if mode == 'w' or mode == 'W':
+                mode = 0
+                write_option_num += 1
+                write_sector_num += offset
+                for j in range(offset):
+                    LSA_has_been_write[s_LSA + j] = True
+            else:
+                mode = 1
+                read_option_num += 1
+                read_sector_num += offset
+                for j in range(offset):
+                    if not LSA_has_been_write[s_LSA + j]:
+                        write_sector_num += 1
+                        read_before_write_sector_num += 1
+                        LSA_has_been_write[s_LSA + j] = True
+            new_row = f"{i * 10} 0 {s_LSA} {offset} {mode}\n"
+            output_file.write(new_row)
+    return write_option_num, write_sector_num, read_option_num, read_sector_num, read_before_write_sector_num
+
 def Simple_to_ASCII(input_file_path, output_file_path, sector_size):
     write_sector_num = 0              # 總共寫入的sector數量(包含read before write)
     write_option_num = 0            # 總共的write trace數量
@@ -24,6 +60,7 @@ def Simple_to_ASCII(input_file_path, output_file_path, sector_size):
                     if s_LSA + j not in LSA_has_been_write:
                         write_sector_num += 1
                         read_before_write_sector_num += 1
+                        LSA_has_been_write.add(s_LSA + j)
             new_row = f"{i * 10} 0 {s_LSA} {offset} {mode}\n"
             output_file.write(new_row)
     return write_option_num, write_sector_num, read_option_num, read_sector_num, read_before_write_sector_num
@@ -39,11 +76,12 @@ def print_result(write_option_num, write_sector_num, read_option_num, read_secto
 if __name__ == "__main__":
     #input_file_path = "trace\ssdTrace-02_extract.txt"
     #output_file_path = "ssdTrace_extract.txt"
-    input_file_path = "trace\ssdTrace-399072.txt"
+    input_file_path = "trace\\ssdtrace_399072.txt"
     output_file_path = "ssdTrace.txt"
     sector_size = 1
 
     result = Simple_to_ASCII(input_file_path, output_file_path, sector_size)
+    #result = Simple_to_ASCII_bitmap(input_file_path, output_file_path, sector_size)
     print_result(*result)
     
 
